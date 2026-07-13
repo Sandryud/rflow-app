d4214c3d3d7305fee54486ab4fd6dba9b85d2016
Sandryud <alyuudin@gmail.com>
2026-07-13 18:17:53 +0300

    feat: add PrismaModule import to ChecklistModule for database integration

--- src/modules/checklist/checklist.module.ts
+++ src/modules/checklist/checklist.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { PrismaModule } from '@database/prisma.module';
 import { ChecklistController } from './checklist.controller';
 import { ChecklistPolicy } from './checklist.policy';
 import { ChecklistRepository } from './checklist.repository';
 import { ChecklistItemService } from './checklist.service';
 
 @Module({
+  imports: [PrismaModule],
   providers: [ChecklistItemService, ChecklistRepository, ChecklistPolicy],
   controllers: [ChecklistController],
 })
